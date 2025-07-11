d4c1ffc29be59295dc293329f93ca6b13594aa8c
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-07-11 23:59:54 +0600

    Update main.py

--- ai-knowledge-base-assistant/main.py
+++ ai-knowledge-base-assistant/main.py
@@ -4,7 +4,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from langchain_core.messages import HumanMessage, AIMessage
-
 from tools import tools
 from assistant_output import AssistantOutput
 
