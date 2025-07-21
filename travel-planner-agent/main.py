6cc5aae1a06b4a407531c86e428cb3fd5f72d79f
Bayzid03 <bayzidhossen06@outlook.com>
2025-07-21 15:56:55 +0600

    Add travel-planner-agent

---
+++ travel-planner-agent/main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv; load_dotenv()
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import tools
+from pathlib import Path
+from datetime import datetime
---
+++ travel-planner-agent/tools.py
@@ -0,0 +1,7 @@
+from langchain.tools import Tool
+from datetime import datetime
+from pathlib import Path
+
+
+
+
