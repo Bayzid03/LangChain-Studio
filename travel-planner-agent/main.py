2b2127f67a6336d83f32635c27d4eccd7ee1abc3
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-07-22 13:13:33 +0600

    Add prompt, agent and chat loop to main.py module

--- travel-planner-agent/main.py
+++ travel-planner-agent/main.py
@@ -7,3 +7,31 @@ from langchain.agents import create_tool_calling_agent, AgentExecutor
 from tools import tools
 from pathlib import Path
 from datetime import datetime
+from prompts import SYSTEM_PROMPT
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.2)
+parser = PydanticOutputParser(pydantic_object=TravelPlan)
+
+prompt= ChatPromptTemplate.from_messages([
+    ("system", SYSTEM_PROMPT),
+    ("placeholder", "{chat_history}"),
+    ("human", "{query}"),
+    ("placeholder", "{agent_scratchpad}"),
+]).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(llm=llm, tools=tools, prompt=prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+chat_history = []
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Exiting the travel planner. Goodbye!")
+        break
+
+    chat_history.append(HumanMessage(content=user_input))
+    
+    response = agent_executor.invoke({
+        "chat_history": chat_history,
+        "query": user_input,
+    })
